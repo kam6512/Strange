067d0f0946e9702f5d5fb31dbf48108d47e11e67
kam6512 <kam6512@gmail.com>
2018-05-16 18:05:50 +0900

    Databinding testing

--- app/src/main/java/com/strange/kam6512/strange/MainActivityKt.kt
+++ app/src/main/java/com/strange/kam6512/strange/MainActivityKt.kt
@@ -9,9 +9,8 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivityKt : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
